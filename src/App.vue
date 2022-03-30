7b1c324aed7405221dd877ba78e063dd8e3f6efa
godlanbo <912300762@qq.com>
2022-03-30 19:20:22 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper flex">
+    <div class="content items-start">
+      <el-button type="primary">Primary</el-button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
