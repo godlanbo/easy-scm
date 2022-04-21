7e9a340286708fa88d63e3021e56ab7e8e48f05c
godlanbo <912300762@qq.com>
2022-04-21 17:51:43 +0800

    feat: 添加arco组件库，vite alias

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router/index'
 import { createPinia } from 'pinia'
 import './style/index.scss'
+import '@arco-design/web-vue/es/message/style/css.js'
+// import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 
 createApp(App).use(router).use(createPinia()).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// Element-Plus components auto import
-import AutoImport from 'unplugin-auto-import/vite'
+// Arco components auto import
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ArcoResolver()],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      views: '/src/views',
+      store: '/src/store',
+      utils: '/src/utils',
+      comp: '/src/components',
+      layout: '/src/layout',
+    },
+  },
 })
