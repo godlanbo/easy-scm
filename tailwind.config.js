0e492bf1dfea168e8d286e7fb0cab389e9645d43
godlanbo <912300762@qq.com>
2022-04-29 19:26:24 +0800

    update：更新自定义tailwind配置和ts配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        card: '0 1px 10px 0 hsla(210, 7%, 22%, .06), 0 2px 4px 0 hsla(210, 7%, 22%, .08)',
+      },
+      colors: {
+        theme: {
+          text: '#2c3f51',
+          primary: '#30849a',
+        },
+      },
+    },
   },
   plugins: [],
 }
