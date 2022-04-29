a8c39229718ed3349b815f4878d79cd4628a786b
godlanbo <912300762@qq.com>
2022-04-29 19:27:06 +0800

    feat：代码预览块组件

---
+++ src/components/PreviewCode.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Prism from 'prismjs'
+import { toRefs } from 'vue'
+const props = defineProps({
+  code: { type: String, default: '' },
+  type: { type: String, default: 'html' },
+})
+const { code, type } = toRefs(props)
+</script>
+<template>
+  <pre
+    class="preview-code-wrapper font-extrabold line-numbers"
+  ><code :class="'language-'+ type" v-html="Prism.highlight(code, Prism.languages[type], type)"></code></pre>
+</template>
+<style lang="scss" scoped>
+.preview-code-wrapper {
+  margin: 0;
+  border-radius: 0;
+  border-bottom-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+}
+</style>
