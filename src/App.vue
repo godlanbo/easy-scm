51a6db15873bfa09758622e25c04674825e878aa
godlanbo <912300762@qq.com>
2022-04-29 19:26:41 +0800

    update：root容器

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="wrapper flex">
-    <div class="content items-start">
-      <el-button type="primary">Primary</el-button>
-    </div>
-  </div>
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss" scoped></style>
