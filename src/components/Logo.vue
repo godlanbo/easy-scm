584d56460f9802b70bf61d2ea196078553e11f66
godlanbo <912300762@qq.com>
2022-04-29 19:26:53 +0800

    feat：logo组件

---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="logo-wrapper cursor-pointer font-black text-3xl">
+    <span class="text-color icon-fire"></span>
+    <span class="text-neutral-800">Easy </span>
+    <span class="text-color">Scm</span>
+  </div>
+</template>
+<style lang="scss" scoped>
+.logo-wrapper {
+  .text-color {
+    background-image: linear-gradient(to bottom, #30849a 50%, #031013);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+  }
+}
+</style>
