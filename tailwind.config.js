4dea7a76c148d528aafaecee925abb5443cd705a
godlanbo <912300762@qq.com>
2022-05-21 14:13:04 +0800

    feat: 自定义更多style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,19 @@ module.exports = {
         card: '0 1px 10px 0 hsla(210, 7%, 22%, .06), 0 2px 4px 0 hsla(210, 7%, 22%, .08)',
       },
       colors: {
+        gray: {
+          light: '#f9fafa',
+          lighter: '#f2f5f8',
+        },
         theme: {
           text: '#2c3f51',
           primary: '#30849a',
+        },
+        border: {
+          gray: '#d0d7de',
+        },
+        black: {
+          default: '#151a1e',
         },
       },
     },
